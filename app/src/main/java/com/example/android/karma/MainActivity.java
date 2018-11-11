fa152ecb66b6e358e7eb6d4b4196a005872071bb
Jojo Masala <jojomasala@protonmail.com>
2018-11-11 14:30:11 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/karma/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.karma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CollapsibleCalendar collapsibleCalendar = findViewById(R.id.collapsibleCalendarView);
+        Calendar today = new GregorianCalendar();
+
+        /*
+        collapsibleCalendar.addEventTag(today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH));
+        today.add(Calendar.DATE,1);
+        collapsibleCalendar.addEventTag(today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH),Color.BLUE);
+        */
+
+        collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
+            @Override
+            public void onDaySelect() {
+
+            }
+
+            @Override
+            public void onItemClick(View v) {
+
+            }
+
+            @Override
+            public void onDataUpdate() {
+
+            }
+
+            @Override
+            public void onMonthChange() {
+
+            }
+
+            @Override
+            public void onWeekChange(int position) {
+
+            }
+        });
+    }
+}
