de3ae9065b718f532979b9b6e8e18b3ea397a46f
Jojo Masala <jojomasala@protonmail.com>
2018-11-13 01:29:02 +0000

    changed the spacing of the material calendar and added a test icon

--- app/src/main/java/com/example/android/karma/MainActivity.java
+++ app/src/main/java/com/example/android/karma/MainActivity.java
@@ -1,53 +1,41 @@
 package com.example.android.karma;
 
-import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity".getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CollapsibleCalendar collapsibleCalendar = findViewById(R.id.collapsibleCalendarView);
-        Calendar today = new GregorianCalendar();
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        try {
+            Log.v(LOG_TAG, "onCreate: setElevation of the action bar");
+            getSupportActionBar().setElevation(0f);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        collapsibleCalendar.addEventTag(today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH));
-        collapsibleCalendar.addEventTag(today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH), Color.BLUE);
+        List<EventDay> events = new ArrayList<>();
 
-        collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
-            @Override
-            public void onDaySelect() {
-            }
+        Calendar calendar = Calendar.getInstance();
+        events.add(new EventDay(calendar, R.drawable.test_event));
 
-            @Override
-            public void onItemClick(View v) {
-
-            }
-
-            @Override
-            public void onDataUpdate() {
-
-            }
-
-            @Override
-            public void onMonthChange() {
-                collapsibleCalendar.setSelectedItemBackgroundColor(getResources().getColor(R.color.colorAccent ));
-
-            }
-
-            @Override
-            public void onWeekChange(int position) {
-
-            }
-        });
+        calendarView.setEvents(events);
     }
 }
